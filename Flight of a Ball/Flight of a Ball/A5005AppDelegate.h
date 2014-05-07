edda66ebdfa3859d2901c676865c2cc27b8524cd
Anthony <li.anthony917@gmail.com>
2014-05-06 20:43:38 -0400

    Initial Xcode commit.

---
+++ Flight of a Ball/Flight of a Ball/A5005AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  A5005AppDelegate.h
+//  Flight of a Ball
+//
+//  Created by Anthony Li on 5/6/2014.
+//  Copyright (c) 2014 anli5005. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface A5005AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Flight of a Ball/Flight of a BallTests/Flight_of_a_BallTests.m
@@ -0,0 +1,34 @@
+//
+//  Flight_of_a_BallTests.m
+//  Flight of a BallTests
+//
+//  Created by Anthony Li on 5/6/2014.
+//  Copyright (c) 2014 anli5005. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Flight_of_a_BallTests : XCTestCase
+
+@end
+
+@implementation Flight_of_a_BallTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
